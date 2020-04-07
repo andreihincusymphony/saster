a0c026b86c3d06e9d6f915fe98f932ed8f420297
Andrei Hîncu <51757656+andreihincusymphony@users.noreply.github.com>
2020-04-07 22:07:05 +0200

    Update Author.java

--- Author.java
+++ Author.java
@@ -12,7 +12,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 })
 public class Author {
     
-    //fffssaaffa
+    //fffssaaffaaaa
 
     @JsonProperty("name")
     private String name;
